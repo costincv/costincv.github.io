00504e143d41e799c14956823b10b776155fdbfb
costincv <48765927+costincv@users.noreply.github.com>
2019-03-31 13:26:27 +0300

    Add files via upload

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -74,7 +74,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"Programator.", "Planificator.", "Vânzător.", "Inovativ!"
+				"Programmer.", "Planner.", "Seller.", "Innovative!"
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
